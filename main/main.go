1f7745e0697feaade01b892f4247e83524c32f2e
Jake Patterson <jacmpatterson@gmail.com>
2018-06-16 00:24:00 -0500

    Cleaned up some of the n go routines channel thing using waitgroups,

--- main/main.go
+++ main/main.go
@@ -8,9 +8,21 @@ import (
 
 func main() {
 	fmt.Println("Hello world")
+	runChansGetPing()
+	runChansGetPingNTimes(10)
+}
+
+func runChansGetPing() {
 	messages := make(chan string)
 	go chans.GetPing(messages)
 	msg := <-messages
 	fmt.Println(msg)
+}
 
+func runChansGetPingNTimes(num int) {
+	messages := make(chan string)
+	go chans.GetPingNTimes(messages, num)
+	for msg := range messages {
+		fmt.Println(msg)
+	}
 }
