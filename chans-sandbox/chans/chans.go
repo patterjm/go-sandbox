f6dd3312733cc43fbe55569f6333776a6fa7020b
Jake Patterson <jacmpatterson@gmail.com>
2019-02-21 21:26:29 -0600

    Add buffered ping example and some thread sleeps to parse console output in a more 'human' way

--- chans-sandbox/chans/chans.go
+++ chans-sandbox/chans/chans.go
@@ -1,7 +1,9 @@
 package chans
 
 import (
+	"fmt"
 	"sync"
+	"time"
 )
 
 //GetPing Adds string "ping" to given channel
@@ -9,18 +11,28 @@ func GetPing(messages chan (string)) {
 	messages <- "ping"
 }
 
+//GetPong Adds string "pong" to given channel
+func GetPong(messages chan (string)) {
+	messages <- "pong"
+}
+
 //GetPingNTimes Adds string "ping" to given channel by creating n go routines and adding 1 message per goroutine
 func GetPingNTimes(messages chan (string), threads int) {
 	var wg sync.WaitGroup
 	wg.Add(threads)
 	for i := 0; i < threads; i++ {
-		go getPingWithWG(messages, &wg)
+		go getPingWithWG(messages, i, &wg)
 	}
 	wg.Wait()
 	close(messages)
 }
 
-func getPingWithWG(messages chan (string), waitGroup *sync.WaitGroup) {
-	GetPing(messages)
+func getPingWithWG(messages chan (string), threadNum int, waitGroup *sync.WaitGroup) {
+	time.Sleep(3000 * time.Millisecond)
+	getPingWithThreadNum(messages, threadNum)
 	waitGroup.Done()
+}
+
+func getPingWithThreadNum(messages chan (string), threadNum int) {
+	messages <- fmt.Sprintf("Wait group chan %s thread: %d", "ping", threadNum)
 }
--- chans-sandbox/main/main.go
+++ chans-sandbox/main/main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/patterjm/go-sandbox/chans-sandbox/chans"
 )
 
 func main() {
 
+	fmt.Println("Adding to and reading one message from a channel")
+	time.Sleep(750 * time.Millisecond)
 	runChansGetPing()
+
+	time.Sleep(500 * time.Millisecond)
+
+	fmt.Println("Add and read two messages in a buffered channel")
+	time.Sleep(750 * time.Millisecond)
+	runChansGetBufferedPings()
+
+	time.Sleep(500 * time.Millisecond)
+
+	fmt.Println("Spawning 10 worker threads and using a waitgroup to coordinate closing the channel")
+	time.Sleep(750 * time.Millisecond)
 	runChansGetPingNTimes(10)
 }
 
@@ -16,6 +30,16 @@ func runChansGetPing() {
 	messages := make(chan string)
 	go chans.GetPing(messages)
 	msg := <-messages
+	fmt.Println(msg)
+}
+
+func runChansGetBufferedPings() {
+	messages := make(chan string, 2)
+	go chans.GetPing(messages)
+	go chans.GetPong(messages)
+	msg := <-messages
+	fmt.Println(msg)
+	msg = <-messages
 	fmt.Println(msg)
 }
 
